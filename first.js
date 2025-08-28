16cc247f052c7928ef11e907e29dfb639ae337f1
saimun212 <saimunislam00402@gmail.com>
2025-08-28 13:37:34 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,39 @@
+/*******************************************************************************************
+                       ***************=== module-13 ===*******
+ ******************************************************************************************
+
+ // ****************variable declaration ****************************
+
+ first var tar por  je  inpute (any name )   tar name  tar por suman  chinno ( = ) tar por value ba jekono  man   ( number ba name ) tar tor semi colone (;)
+
+ example: var string = 'saimun';
+  
+ var declaration korar por   var  declaration korar somay je name used korci oi neme ba number dibo and semi clone dibo 
+
+ example: var string;
+
+ ar por output payar jonno amike seter potome console.log dite hobe tar por first braket () dite hobe ai first braketer vitor je var declaration korar somay je name used korci oi ta used korte hobe and semi colone dite hobe and kaj ses 
+
+ example: console.log(string);
+
+ */
+
+//variable declaration //
+
+ var weight = 38; 
+ var price = 38;
+ var year = 1995;
+ var money = 2010;
+var age;
+console.log(money);
+console.log(price);
+console.log(year);
+console.log(weight);
+console.log(name);
+
+
+
+var  name = anonymous;
+
+var string;
+console.log(name)
